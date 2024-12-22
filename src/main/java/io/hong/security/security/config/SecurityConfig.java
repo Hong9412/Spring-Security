c1c5576d87fee107c23f483f922e60c5a118bfa0
Hong9412 <rudfmr1235@naver.com>
2024-12-22 16:47:06 +0900

    [ADD] 로그인 검증, 로그아웃 기능 추가
     로그인 검증 -> authenticationProvider
               -> UserDetailsService
     로그아웃 기능 추가 -> Get방식

--- src/main/java/io/hong/security/security/config/SecurityConfig.java
+++ src/main/java/io/hong/security/security/config/SecurityConfig.java
@@ -1,7 +1,10 @@
-package io.hong.security.config;
+package io.hong.security.security.config;
 
+import io.hong.security.users.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,7 +18,10 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final AuthenticationProvider authenticationProvider;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -24,19 +30,9 @@ public class SecurityConfig {
                         .requestMatchers("/css/**", "/images/**", "/js/**", "/favicon.*", "/*/icon-*").permitAll() // 정적 자원 설정
                         .requestMatchers("/","/join").permitAll() // 해당 경로에 대해서 모두 접근 가능
                         .anyRequest().authenticated()) // 해당 경로에 대해서 인증을 받아야 접근 가능
-                .formLogin(form -> form.loginPage("/login").permitAll()) // 커스텀 로그인 페이지 설정
+                .formLogin(form -> form.loginPage("/login").permitAll())
+                .authenticationProvider(authenticationProvider)
         ;
         return http.build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService(){
-        UserDetails user = User.withUsername("hong").password("{noop}1234").roles("USER").build();
-        return  new InMemoryUserDetailsManager(user);
     }
 }
